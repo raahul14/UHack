a37da34d07e20e0391280b3bef9aa12210b00855
Rahul Mittal <rahul.mittal1499@gmail.com>
2017-12-03 01:20:35 -0500

    CNAME added

---
+++ backend.java
@@ -0,0 +1,15 @@
+package hack;
+public class BackEnd
+{
+
+
+  public class BackEnd()
+  {
+
+  }
+
+
+ public static void main(String[] args) {
+
+  }
+}
---
+++ user.java
@@ -0,0 +1,8 @@
+package hack;
+public class User{
+
+public static User;
+public static String[] interests;
+public static boolean createUser()
+
+}
